40d9583121facad0893d7a4ef292c4252708e92c
Rohitchaudhary2 <rohitbhambota1@gmail.com>
2024-09-05 18:34:43 +0530

    Template-driven form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,45 @@
-import { Component } from '@angular/core';
+import { afterNextRender, Component, DestroyRef, inject, viewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  private form = viewChild.required<NgForm>('form')
+  private destroyRef = inject(DestroyRef)
+
+  
+  
+  constructor(){
+    afterNextRender(() => {
+      const savedItem = window.localStorage.getItem('saved-login-form')
+      if(savedItem){
+        const loadedFormData = JSON.parse(savedItem)
+        const savedEmail = loadedFormData.email
+
+        setTimeout(() => {
+          this.form().controls['email'].setValue(savedEmail)
+        }, 1)
+      }
+      
+      const subscription = this.form().valueChanges?.pipe(debounceTime(500)).subscribe({
+        next: (value) => window.localStorage.setItem('saved-login-form', JSON.stringify({email: value.email}))
+      })
+      this.destroyRef.onDestroy(() => subscription?.unsubscribe())
+    })
+
+  }
+
+  onSubmit(formData: NgForm){
+    if(formData.form.invalid) return;
+
+    const enteredEmail = formData.form.value.email
+    const enteredPassword = formData.form.value.password
+    console.log(enteredEmail, enteredPassword)
+  }
+}
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
 })
-export class SignupComponent {}
+export class SignupComponent {
+  form = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email]
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)]
+    })
+  })
+
+  get isValidEmail(){
+    return (this.form.controls.email.touched && this.form.controls.email.dirty && this.form.controls.email.invalid)
+  }
+
+  get isValidPassword(){
+    return (this.form.controls.password.touched && this.form.controls.password.dirty && this.form.controls.password.invalid)
+  }
+
+  onSubmit(){
+    
+  }
+}
