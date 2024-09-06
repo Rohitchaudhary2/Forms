faa766a4866d3ba733746be39c026b962edaa2a2
Rohitchaudhary2 <rohitbhambota1@gmail.com>
2024-09-06 19:56:24 +0530

    reactive forms completed

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,6 +1,18 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
+function equalValues(control1: string, control2: string){
+  return (control: AbstractControl) => {
+    const val1 = control.get(control1)?.value
+    const val2 = control.get(control2)?.value
+
+    if(val1 === val2) return null
+
+    return {
+      'valuesNotEqual': true
+    }
+  }
+}
 @Component({
   selector: 'app-signup',
   standalone: true,
@@ -13,9 +25,32 @@ export class SignupComponent {
     email: new FormControl('', {
       validators: [Validators.required, Validators.email]
     }),
-    password: new FormControl('', {
+    passwords: new FormGroup({
+      password: new FormControl('', {
       validators: [Validators.required, Validators.minLength(6)]
-    })
+    }),
+    confirmPassword: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)]
+    }),
+    }, {validators: [equalValues('password', 'confirmPassword')]}),
+    
+    firstName: new FormControl('', {validators: [Validators.required]}),
+    lastName: new FormControl('', {validators: [Validators.required]}),
+    address: new FormGroup({
+      street: new FormControl('', {validators: [Validators.required]}),
+      number: new FormControl('', {validators: [Validators.required]}),
+      postalCode: new FormControl('', {validators: [Validators.required]}),
+      city: new FormControl('', {validators: [Validators.required]}),
+    }),
+    role: new FormControl<
+      'student' | 'teacher' | 'employee' | 'founder' | 'other'
+    >('student', { validators: [Validators.required] } ),
+    source: new FormArray([
+      new FormControl(false),
+      new FormControl(false),
+      new FormControl(false)
+    ]),
+    agree: new FormControl(false, {validators: Validators.required})
   })
 
   get isValidEmail(){
@@ -23,10 +58,22 @@ export class SignupComponent {
   }
 
   get isValidPassword(){
-    return (this.form.controls.password.touched && this.form.controls.password.dirty && this.form.controls.password.invalid)
+    return (this.form.controls.passwords.controls.password.touched && this.form.controls.passwords.controls.password.dirty && this.form.controls.passwords.controls.password.invalid)
   }
 
   onSubmit(){
-    
+    if(this.form.invalid || !this.form.value.agree) {
+      console.log('Invalid')
+      return;
+    }
+    console.log(this.form.value)
+    console.log(this.form.value.email)
+    console.log(this.form.value.passwords?.password)
+    this.form.reset()
+    this.form.controls.role.setValue("student")
+  }
+
+  onReset() {
+    this.form.reset()
   }
 }
